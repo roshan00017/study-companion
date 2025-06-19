5f6ab05f0f957d8dd5b263cdf3383b911126a78c
roshan <roshangyawali17@gmail.com>
2025-06-19 15:28:42 +0545

    remove: unused imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "react-quill-new/dist/quill.snow.css";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "./context/AuthContext.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
